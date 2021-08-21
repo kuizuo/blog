1c6f794c02fa5529cb55a068e0b5841975e12767
kuizuo <kuizuo12@163.com>
2021-08-21 20:21:00 +0800

    fix: 更改百度推送

--- src/plugin/plugin-baidu-analytics/index.js
+++ src/plugin/plugin-baidu-analytics/index.js
@@ -10,7 +10,7 @@ module.exports = function (context, options) {
             var _hmt = _hmt || [];
             (function() {
               var hm = document.createElement("script");
-              hm.src = "https://hm.baidu.com/hm.js?135f3e7db7162b348a790b8b00e07413";
+              hm.src = "https://hm.baidu.com/hm.js?c9a3849aa75f9c4a4e65f846cd1a5155";
               hm.defer = true;
               var s = document.getElementsByTagName("script")[0]; 
               s.parentNode.insertBefore(hm, s);
@@ -21,7 +21,7 @@ module.exports = function (context, options) {
             tagName: "meta",
             attributes: {
               name: "baidu-site-verification",
-              content: "IXU12YQUjF",
+              content: "code-rqLUw5reVS",
             },
           },
         ],
