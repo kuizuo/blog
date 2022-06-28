1fe587b441858fec79125e6c481644b5a42c380f
kuizuo <kuizuo12@163.com>
2022-06-28 18:15:11 +0800

    fix: 更新about页面

--- src/theme/MDXPage/index.tsx
+++ src/theme/MDXPage/index.tsx
@@ -1,39 +1,46 @@
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import { MDXProvider } from '@mdx-js/react';
-import MDXComponents from '@theme/MDXComponents';
-import type { Props } from '@theme/MDXPage';
-import TOC from '@theme/TOC';
-import { ThemeClassNames } from '@docusaurus/theme-common';
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 
-import styles from './styles.module.css';
+import React from 'react'
+import clsx from 'clsx'
+import { PageMetadata, HtmlClassNameProvider, ThemeClassNames } from '@docusaurus/theme-common'
+import Layout from '@theme/Layout'
+import MDXContent from '@theme/MDXContent'
+import TOC from '@theme/TOC'
+import type { Props } from '@theme/MDXPage'
 
-function MDXPage(props: Props) {
-  const { content: MDXPageContent } = props;
-  const { frontMatter, metadata } = MDXPageContent;
+import styles from './styles.module.css'
 
-  const { title, description, wrapperClassName, hide_table_of_contents: hideTableOfContents } = frontMatter;
-  const { permalink } = metadata;
+export default function MDXPage(props: Props): JSX.Element {
+  const { content: MDXPageContent } = props
+  const {
+    metadata: { title, description, frontMatter },
+  } = MDXPageContent
+  const { wrapperClassName, hide_table_of_contents: hideTableOfContents } = frontMatter
 
   return (
-    <Layout title={title} description={description} permalink={permalink} wrapperClassName={wrapperClassName ?? ThemeClassNames.wrapper.mdxPages} pageClassName={ThemeClassNames.page.mdxPage}>
-      <main className='container container--fluid margin-vert--lg'>
-        <div className={clsx('row', styles.mdxPageWrapper)}>
-          <div className={clsx('col', 'col--8', styles.content)}>
-            <MDXProvider components={MDXComponents}>
-              <MDXPageContent />
-            </MDXProvider>
-          </div>
-          {!hideTableOfContents && MDXPageContent.toc && (
-            <div className='col col--2'>
-              <TOC toc={MDXPageContent.toc} />
+    <HtmlClassNameProvider className={clsx(wrapperClassName ?? ThemeClassNames.wrapper.mdxPages, ThemeClassNames.page.mdxPage)}>
+      <PageMetadata title={title} description={description} />
+      <Layout>
+        <main className='container container--fluid margin-vert--lg'>
+          <div className={clsx('row', styles.mdxPageWrapper)}>
+            <div className={clsx('col', 'col--8')}>
+              <MDXContent>
+                <MDXPageContent />
+              </MDXContent>
             </div>
-          )}
-        </div>
-      </main>
-    </Layout>
-  );
+            {!hideTableOfContents && MDXPageContent.toc && (
+              <div className='col col--2'>
+                <TOC toc={MDXPageContent.toc} minHeadingLevel={frontMatter.toc_min_heading_level} maxHeadingLevel={frontMatter.toc_max_heading_level} />
+              </div>
+            )}
+          </div>
+        </main>
+      </Layout>
+    </HtmlClassNameProvider>
+  )
 }
-
-export default MDXPage;
