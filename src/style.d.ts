d4b685bfd264441c9d5103ffbf8cab7fa4d1944c
Kuizuo <hi@kuizuo.cn>
2023-12-26 02:53:18 +0800

    feat: optimize type

--- src/style.d.ts
+++ src/style.d.ts
@@ -2,3 +2,8 @@ declare module '*.module.scss' {
   const classes: { readonly [key: string]: string }
   export default classes
 }
+
+declare module '*.scss' {
+  const src: string
+  export default src
+}
