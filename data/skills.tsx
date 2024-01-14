2ec7763627b4079c18421a5d876347fba2a14837
Kuizuo <hi@kuizuo.cn>
2024-01-14 10:10:27 +0800

    chore: update skill

--- data/skills.tsx
+++ data/skills.tsx
@@ -21,25 +21,33 @@ const SKILLS: IconProps[] = [
 
   {
     icon: 'logos:javascript',
-    style: { top: '5%', left: '52%' },
+    style: { top: '5%', left: '54%' },
   },
   {
     icon: 'logos:typescript-icon',
-    style: { top: '9%', left: '58%' },
+    style: { top: '9%', left: '60%' },
   },
 
   {
     icon: 'logos:nodejs-icon-alt',
-    style: { top: '15%', left: '30%' },
+    style: { top: '14%', left: '30%' },
   },
   {
     icon: 'logos:nestjs',
-    style: { top: '20%', left: '38%' },
+    style: { top: '19%', left: '38%' },
+  },
+  {
+    icon: 'logos:prisma',
+    style: { top: '24%', left: '50%' },
+  },
+  {
+    icon: 'logos:postgresql',
+    style: { top: '26%', left: '60%' },
   },
 
   {
     icon: 'logos:tailwindcss-icon',
-    style: { top: '30%', left: '70%' },
+    style: { top: '30%', left: '90%' },
   },
 
   {
