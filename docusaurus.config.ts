0ac3c33a1cdd477d2623d596b71e3db32ee8d6a5
Kuizuo <hi@kuizuo.cn>
2024-01-17 19:22:12 +0800

    feat: add @vercel/analytics

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -198,7 +198,7 @@ const config: Config = {
           trackingID: 'G-S4SD5NXWXF',
           anonymizeIP: true,
         },
-        // debug: true,
+        debug: process.env.NODE_ENV === 'development',
       } satisfies Preset.Options,
     ],
   ],
@@ -233,7 +233,7 @@ const config: Config = {
     [
       '@docusaurus/plugin-pwa',
       {
-        debug: true,
+        debug: process.env.NODE_ENV === 'development',
         offlineModeActivationStrategies: ['appInstalled', 'standalone', 'queryString'],
         pwaHead: [
           { tagName: 'link', rel: 'icon', href: '/img/logo.png' },
--- src/plugin/plugin-vercel-analytics/index.js
+++ src/plugin/plugin-vercel-analytics/index.js
@@ -2,12 +2,23 @@ import { inject } from '@vercel/analytics';
 
 export default async function vercelAnalytics(context, options) {
   return {
-    name: 'vercel-analytics',
-    async loadContent() {
-      inject();
+    name: 'docusaurus-plugin-vercel-analytics',
+    injectHtmlTags() {
+      if (process.env.NODE_ENV === 'development') {
+        return {}
+      }
+
+      return {
+        headTags: [
+          {
+            tagName: 'script',
+            innerHTML: `
+            window.va = window.va || function () { (window.vaq = window.vaq || []).push(arguments); };
+           `,
+          },
+          { tagName: 'script', defer: true, src: '/_vercel/insights/script.js' },
+        ],
+      }
     },
-    async contentLoaded({ content, actions }) {
-    },
-    /* other lifecycle API */
   };
 }
