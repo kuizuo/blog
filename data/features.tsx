0df1a24efddd8fa29e39f0011714ae531da6f434
Kuizuo <hi@kuizuo.cn>
2024-06-17 19:44:45 +0800

    chore: update SVG headers to use responsive height in features.tsx

--- data/features.tsx
+++ data/features.tsx
@@ -22,7 +22,7 @@ const FEATURES: FeatureItem[] = [
         作为一名 TypeScript 全栈工程师，秉着能用 TS 绝不用 JS 的原则，为项目提供类型安全的保障，提高代码质量和开发效率。
       </Translate>
     ),
-    header: <WebDeveloperSvg className={'h-[150px] w-full'} role="img" />,
+    header: <WebDeveloperSvg className={'h-auto md:h-[150px] w-full'} height={150} role="img" />,
     icon: <Icon icon="logos:typescript-icon" className="h-4 w-4 text-neutral-500" />,
   },
   {
@@ -35,7 +35,7 @@ const FEATURES: FeatureItem[] = [
         作为一名曾学习与实践逆向工程两年半的开发者，对于逆向工程有着浓厚的兴趣，同时造就了超凡的阅读代码能力。没有看不懂的代码，只有不想看的代码。
       </Translate>
     ),
-    header: <SpiderSvg className={'h-[150px] w-full'} role="img" />,
+    header: <SpiderSvg className={'h-auto md:h-[150px] w-full'} height={150} role="img" />,
   },
   {
     title: translate({
@@ -47,7 +47,7 @@ const FEATURES: FeatureItem[] = [
         作为一名开源爱好者，积极参与开源社区，为开源项目贡献代码，希望有生之年能够构建出一个知名的开源项目。
       </Translate>
     ),
-    header: <OpenSourceSvg className={'h-[150px] w-full'} role="img" />,
+    header: <OpenSourceSvg className={'h-auto md:h-[150px] w-full'} height={150} role="img" />,
   },
 ]
 
