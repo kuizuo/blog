0b453aa9c4394254464484e316b28ed2a87221a5
Kuizuo <hi@kuizuo.cn>
2025-12-21 15:20:45 +0800

    chore: update social media links

--- data/social.ts
+++ data/social.ts
@@ -19,12 +19,12 @@ type SocialValue = {
 
 const social: Social = {
   github: 'https://github.com/kuizuo',
-  x: 'https://twitter.com/kuizuo',
+  x: 'https://x.com/ku1zu0',
   juejin: 'https://juejin.cn/user/1565318510545901',
   wx: 'https://img.kuizuo.me/wechat.png',
   // qq: 'https://img.kuizuo.me/qq.png',
   // zhihu: 'https://www.zhihu.com/people/kuizuo',
-  cloudmusic: 'https://music.163.com/#/user/home?id=1333010742',
+  // cloudmusic: 'https://music.163.com/#/user/home?id=1333010742',
   email: 'mailto:hi@kuizuo.me',
   discord: 'https://discord.gg/M8cVcjDxkz',
 }
